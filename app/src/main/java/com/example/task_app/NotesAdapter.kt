790b078d424396b3bb818aea68b702b1bacd545c
Vihanga Upamal <IT22120502@my.sliit.lk>
2024-05-14 14:45:29 +0530

    add scroll feature

--- app/src/main/java/com/example/task_app/NotesAdapter.kt
+++ app/src/main/java/com/example/task_app/NotesAdapter.kt
@@ -2,6 +2,7 @@ package com.example.task_app
 
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -20,6 +21,7 @@ class NotesAdapter(private var notes: List<Note>, private val context: Context) 
         val contentTextView: TextView = itemView.findViewById(R.id.contentTextView)
         val updateButton: ImageView = itemView.findViewById(R.id.updateButton)
         val deleteButton: ImageView = itemView.findViewById(R.id.deleteButton)
+        val noteImageView: ImageView = itemView.findViewById(R.id.noteImageView)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
@@ -35,6 +37,13 @@ class NotesAdapter(private var notes: List<Note>, private val context: Context) 
         val note = notes[position]
         holder.titleTextView.text = note.title
         holder.contentTextView.text = note.content
+
+        if (!note.imagePath.isNullOrEmpty()) {
+            holder.noteImageView.visibility = View.VISIBLE
+            holder.noteImageView.setImageURI(Uri.parse(note.imagePath))
+        }else {
+            holder.noteImageView.visibility = View.GONE
+        }
 
         holder.updateButton.setOnClickListener{
             val intent = Intent(holder.itemView.context, UpdateNoteActivity::class.java).apply {
